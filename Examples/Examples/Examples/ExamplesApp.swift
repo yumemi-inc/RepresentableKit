5a267b078158200dfd06be888701329c290eb3d4
Mikhail Apurin <m_apurin@yumemi.co.jp>
2021-12-03 19:11:23 +0900

    Add examples project

---
+++ Examples/Examples/Examples/ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplesApp.swift
+//  Examples
+//
+//  Created by Mikhail Apurin on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
