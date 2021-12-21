229250361c2f920c7d074a91a73e00abaca5f90d
Mikhail Apurin <m_apurin@yumemi.co.jp>
2021-12-21 22:25:49 +0900

    Set minimum iOS version to 11 to enable weak linking

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RepresentableKit",
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "RepresentableKit",
